ef456f37ca228275f4392ca5ee5ed5d39d81f752
samira ouraga <samiraob71@gmail.com>
2017-09-02 13:01:42 -0700

    average,sum, multiples, odd in python

---
+++ Python/practice2.py
@@ -0,0 +1,22 @@
+#Write code that prints all the odd numbers from 1 to 1000
+
+# for i in range(1, 1000, 2):
+#     print(i)
+
+#Create another program that prints all the multiples of 5 from 5 to 1,000,000
+# def Themultiples():
+#     for i in range(5,1000000):
+#         if i%5 == 0:
+#             print i
+
+
+#sum of values in list
+# a = [1, 2, 5,87,9,45]
+# b = sum(a)
+# print b 
+
+#the average
+# a = [1, 2, 5,87,9,45]
+# print  sum(a, 0.0) / len(a)
+
+
